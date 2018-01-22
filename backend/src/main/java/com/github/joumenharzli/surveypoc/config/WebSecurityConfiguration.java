74a0e505f58afe315b7ba6999499d92e95e238c2
HARZLI Joumen <harzli.joumen@gmail.com>
2018-01-22 19:17:52 +0100

    Enable Cors

--- backend/src/main/java/com/github/joumenharzli/surveypoc/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/github/joumenharzli/surveypoc/config/WebSecurityConfiguration.java
@@ -15,10 +15,14 @@
 
 package com.github.joumenharzli.surveypoc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * WebSecurity Configuration
@@ -43,9 +47,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
    */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
+    http
+        .cors()
+        .and()
+        .csrf()
         .disable()
         .authorizeRequests()
         .antMatchers("/v2/api-docs/**", "/swagger-resources/configuration/ui", "/swagger-ui.html").permitAll();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
